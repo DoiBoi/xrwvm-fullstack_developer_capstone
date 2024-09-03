0a0f2e587aa1c02f2fa16f389e4dffd91659235b
DoiBoi <JordanLiaudrix@gmail.com>
2024-09-02 17:31:18 -0700

    More formatting changes

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -50,15 +50,16 @@ class CarModel(models.Model):
         (COUPE, "Coupe"),
         (TRUCK, "Truck"),
     ]
-    type = models.CharField(max_length=20, choices=CAR_TYPE_CHOICES, default='SUV')
-    year = models.IntegerField(default=2023, validators=[
-            MaxValueValidator(2023),
-            MinValueValidator(2015)
-        ])
+    type = models.CharField(max_length=20,
+                            choices=CAR_TYPE_CHOICES, default='SUV')
+    year = models.IntegerField(default=2023,
+                               validators=[
+                                   MaxValueValidator(2023),
+                                   MinValueValidator(2015)
+                                   ])
     color = models.CharField(max_length=50, blank=True, null=True)
     price = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
     # Other fields as needed
 
     def __str__(self):
         return self.name  # Return the name as the string representation
-
